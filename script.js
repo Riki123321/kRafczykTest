b297d38a9c5a22d3de243b37669e7bd93754032e
kRafczyk <rafidiego69@gmail.com>
2020-05-26 20:37:03 +0200

    Add footer and little improvement CSS

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var cos = [2,21,12,10,44,66];
-
-cos.sort(function(a, b){
-    return a-b;
-})
+$(document).ready(function () { 
+    let widthWindowPoint = 757; 
+    if (window.innerWidth < widthWindowPoint) { 
+        $('.footer-nav__block').find('.list-block__head').click(function (e) {
+            $(this).next().slideToggle();
+            $(".list-block__list").not($(this).next()).slideUp();
+        });
+        $('.list-block-head').on('click', function (e) {
+            e.preventDefault();
+        });
+    }
+});
