14145dbefd0b67f47935cf8714ccfa9289e3ac4f
kRafczyk <rafidiego69@gmail.com>
2020-05-29 08:26:40 +0200

    Add resnosive header

--- script.js
+++ script.js
@@ -27,21 +27,73 @@ $(document).ready(function () {
         if ($(this).is(".type-one-gallery-slides")) {
             $(this).slick({
             infinite: true,
-            slidesToShow: slidesToshowOneType,
+            slidesToShow: 6,
             slidesToScroll: 1,
-            prevArrow: $('.arrow-prev-producent'), 
-            nextArrow: $('.arrow-next-producent'), 
+            prevArrow: $('.arrow-prev-producent'),
+            nextArrow: $('.arrow-next-producent'),
+            responsive: [
+                {
+                  breakpoint: 757,
+                  settings: {
+                    slidesToShow: 4, 
+                  }
+                },
+                {
+                    breakpoint: 400,
+                    settings: {
+                      slidesToShow: 2, 
+                    }
+                  }
+            ]
         });
         } 
         else if ($(this).is(".type-two-gallery-slides")){
-        $(this).slick({
-            infinite: true,
+        $(this).slick({ 
             slidesToScroll: 1,
-            slidesToShow: slidesToshowTwoType,
-            prevArrow: $('.arrow-prev'),
-            nextArrow: $('.arrow-next'),
+            slidesToShow: 4, 
+            prevArrow: $('.arrow-prev-product'),
+            nextArrow: $('.arrow-next-product'),
+            responsive: [
+                {
+                  breakpoint: 1200,
+                  settings: {
+                    slidesToShow: 2,
+                    
+                  }
+                },
+                {
+                    breakpoint: 400,
+                    settings: {
+                      slidesToShow: 1,
+                      arrows: false,
+                    }
+                  }
+            ]
         });
         }
+        else if ($(this).is(".type-three-gallery-slides")){
+            $(this).slick({
+                infinite: true,
+                slidesToScroll: 1,
+                slidesToShow: 3,
+                prevArrow: $('.arrow-prev'),
+                nextArrow: $('.arrow-next'),
+                responsive: [
+                    {
+                      breakpoint: 1200,
+                      settings: {
+                        slidesToShow: 2,
+                      }
+                    },
+                    {
+                        breakpoint: 400,
+                        settings: {
+                          slidesToShow: 1,
+                        }
+                      }
+                ]
+            });
+            }
         else {
         $(this).slick();
         }
