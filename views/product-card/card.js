2ccab2f8793465a26224359d3538e65ec40cd158
kRafczyk <rafidiego69@gmail.com>
2020-06-03 09:47:53 +0200

    Add custom select

--- views/product-card/card.js
+++ views/product-card/card.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+  const selected = document.querySelector(".selected");
+  const optionsContainer = document.querySelector(".options-container");
+  
+  const optionsList = document.querySelectorAll(".option");
+  
+  selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+  });
+  
+  optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+      selected.innerHTML = o.querySelector("label").innerHTML;
+      optionsContainer.classList.remove("active");
+    });
+  });
 
   var Carousel = $(".slider");
 
@@ -43,6 +58,9 @@ $(document).ready(function () {
       });
     }
   });
+
+
+
 
 });
 
